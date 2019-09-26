295345782e57742c072946b6e0aeb58e68748f3d
avinogradov <alexandru.vinogradov@amadeus.com>
2019-09-26 17:00:03 +0200

    Improved get random safe position

--- src/main/java/com/player/BomberBehaviour.java
+++ src/main/java/com/player/BomberBehaviour.java
@@ -32,31 +32,36 @@ class BomberBehaviour extends EntityBehaviour {
 
     // move
     if (entity.item == EntityType.TRAP) {
-      Cell closestOreCell = this.getClosestInZoneOreCell();
-      return returnAction(Action.move(closestOreCell.coord));
+      Coord randomFreeCoord = getNextTrapTarget(
+              5,
+              board.getWidth() - 3,
+              4,
+              board.getHeight() - 3,
+              0);
+      return returnAction(Action.move(randomFreeCoord));
     }
 
     // Return to headquarters for the new RADAR
     return returnAction(Action.move(getCloserHeadQuarterCell().coord));
   }
-  
+
+  private Coord getNextTrapTarget(int startX, int endX, int startY, int endY, int deep) {
+    Coord coord = getRandomCoord(startX, endX, startY, endY);
+    if (isCoordOutsideTrapCoverrage(coord)
+            && !isCellBad(board.getCell(coord)) && board.getCell(coord).hasOre()){
+        return coord;
+    } else {
+      // Should not happen
+      if (deep >= 30) {
+        return new Coord(0, 0);
+      }
+    }
+    return getNextTrapTarget(startX, endX, startY, endY, deep + 1);
+  }
+
   private boolean isCoordOutsideTrapCoverrage(final Coord coord) {
     return this.board.myTrapPos.stream()
             .noneMatch(rCoord ->
                     isInside(rCoord.x, rCoord.y, 2, coord.x, coord.y));
-  }
-
-  Cell getClosestInZoneOreCell() {
-    Cell closerCell = board.getCell(this.getRandomSafeCoord(0, board.getWidth(), 0, board.getHeight()));
-    int minDistance = 50;
-    for (final Cell cell : board.getCells()) {
-      int distance = cell.coord.distance(this.entity.pos);
-      if (cell.hasOre() && distance < minDistance && !isCellBad(cell)
-          && isInsideRadarOrTrapZone()) {
-        closerCell = cell;
-        minDistance = distance;
-      }
-    }
-    return closerCell;
   }
 }

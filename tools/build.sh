3cf0b91558922fc684ab64bd9a84fe3099a494e5
anna-kozachenko <a.kozachenko91@gmail.com>
2019-10-04 19:33:14 +0200

    Fix build script

--- tools/build.sh
+++ tools/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ -f target/player.java ]
 then
     rm target/player.java
@@ -5,7 +7,7 @@ fi
 
 # Add all classes to file + remove all imports + remove public keyword from classes
 for f in ./src/main/java/com/player/*/**; do tail -n +1 "$f" >> target/player.java; done
-sed -i.bak "s/public class/class/;s/public abstract class/abstract class/;/^import/d" target/player.java
+sed -i.bak "s/public class/class/;s/public enum/enum/;s/public abstract class/abstract class/;/^import/d" target/player.java
 
 # Add player class content at the beginning of the file
 cat ./src/main/java/com/player/Player.java | cat - target/player.java > temp && mv temp target/player.java

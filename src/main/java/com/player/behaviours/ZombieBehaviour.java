f42eedd131e492358b7451753d3a2134834ba2ec
anna-kozachenko <a.kozachenko91@gmail.com>
2019-10-04 22:04:12 +0200

    Generate Bomber when we have min 3 robots alive

--- src/main/java/com/player/behaviours/ZombieBehaviour.java
+++ src/main/java/com/player/behaviours/ZombieBehaviour.java
@@ -4,11 +4,11 @@ import com.player.model.Action;
 import com.player.model.Board;
 import com.player.model.Entity;
 
-public class LazyBehaviour extends EntityBehaviour {
+public class ZombieBehaviour extends EntityBehaviour {
 
-  public LazyBehaviour(final Entity entity, final Board board) {
+  public ZombieBehaviour(final Entity entity, final Board board) {
     super(entity, board);
-    this.NAME = "Lazy";
+    this.NAME = "Zombie";
   }
 
   @Override public Action getNextAction() {
--- src/main/java/com/player/orchestrator/ClassicBehaviourOrchestrator.java
+++ src/main/java/com/player/orchestrator/ClassicBehaviourOrchestrator.java
@@ -1,6 +1,6 @@
 package com.player.orchestrator;
 
-import com.player.behaviours.LazyBehaviour;
+import com.player.behaviours.ZombieBehaviour;
 import com.player.behaviours.MinerBehaviour;
 import com.player.behaviours.ScoutBehaviour;
 import com.player.behaviours.SuicideBomberBehaviour;
@@ -13,25 +13,38 @@ public class ClassicBehaviourOrchestrator extends BehaviourOrchestrator {
     @Override
     public void setRobotBehaviours() {
         for (final Entity robot : board.getMyTeam().getRobots()) {
-            if (this.behaviourMap.get(robot.getId()) == null) {
-                if (robot.isAlive()) {
-                    if (this.getNumberOfScouts() < 1
-                            && board.getCells().stream().filter(cell -> cell.hasOre()
-                            && !cell.isHole()
-                            && !cell.hasAllyTrap(board)).count() < board.getMyTeam().getNumberOfRobotAlive()
-                            && robot.getId() == getBestMatchNextScoutRobotId().orElse(robot.getId())) {
-                        this.behaviourMap.put(robot.getId(), new ScoutBehaviour(robot, board));
-                    } else if (robot.isAlive() && this.getNumberOfSuicideBombers() < 1
-                            && this.board.getMyTeam().getRobots().stream().filter(Entity::isAlive).count() >= 2
-                            && this.board.getOpponentTeam().getRobots().stream().filter(Entity::isAlive).count() >= 2) {
-                        this.behaviourMap.put(robot.getId(), new SuicideBomberBehaviour(robot, board));
-                    } else {
-                        this.behaviourMap.put(robot.getId(), new MinerBehaviour(robot, board));
-                    }
-                } else {
-                    this.behaviourMap.put(robot.getId(), new LazyBehaviour(robot, board));
-                }
+            if (behaviourMap.get(robot.getId()) != null) {
+                continue;
             }
+
+            if (!robot.isAlive()) {
+                this.behaviourMap.put(robot.getId(), new ZombieBehaviour(robot, board));
+            }
+
+            // If we don't have a Scout
+            if (this.getNumberOfScouts() == 0
+                    // and we don't have much gold left
+                    && board.getCells().stream()
+                        .filter(cell -> cell.hasOre() && !cell.isHole() && !cell.hasAllyTrap(board))
+                        .count() < board.getMyTeam().getNumberOfRobotAlive()
+                    // ...?
+                    && robot.getId() == getBestMatchNextScoutRobotId().orElse(robot.getId())) {
+                this.behaviourMap.put(robot.getId(), new ScoutBehaviour(robot, board));
+                continue;
+            }
+
+            // If we don't have a Bomber
+            if (this.getNumberOfSuicideBombers() == 0
+                    // and we have more than 2 robots alive
+                    && this.board.getMyTeam().getRobots().stream().filter(Entity::isAlive).count() > 2
+                    // and the enemy has more than 2 robots alive
+                    && this.board.getOpponentTeam().getRobots().stream().filter(Entity::isAlive).count() >= 2) {
+                this.behaviourMap.put(robot.getId(), new SuicideBomberBehaviour(robot, board));
+                continue;
+            }
+
+            // Otherwise, it should be a Scout
+            this.behaviourMap.put(robot.getId(), new MinerBehaviour(robot, board));
         }
     }
 
